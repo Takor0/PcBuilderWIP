193e4eec9a494c1a03dfa2897e5a6e96a92dda7a
tomi0 <tomi0985@wp.pl>
2024-11-30 13:40:08 +0100

    [FE] tweak helpers.ts

--- frontend/src/mixins/helpers.ts
+++ frontend/src/mixins/helpers.ts
@@ -1,11 +1,11 @@
 export default {
-    data() {
+    data(): { isDarkMode: boolean } {
         return {
-            isDarkMode: localStorage.getItem('theme') == 'dark'
+            isDarkMode: localStorage.getItem('theme') === 'dark'
         }
     },
     methods: {
-        setMode(isDarkMode: Boolean) {
+        setMode(isDarkMode: boolean) {
             if (isDarkMode) {
                 document.documentElement.classList.add('my-app-dark');
                 localStorage.setItem('theme', 'dark');
