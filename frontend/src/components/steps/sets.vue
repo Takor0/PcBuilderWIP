d14881d39469e7f3e7b2def802b31f858556d764
Tomasz <tomi0985@wp.pl>
2025-01-14 22:40:48 +0100

    [FE] displaying sets

--- frontend/src/components/steps/sets.vue
+++ frontend/src/components/steps/sets.vue
@@ -1,33 +1,86 @@
 <template>
   <div>
+    <Dialog v-model:visible="isSpecDialog" modal header="Dodatkowe informacje " :style="{ width: '25rem' }">
+      <div v-for="val, key in currentSpec">
+        <strong>
+          {{ key }}:
+        </strong>
+        <span>{{ val }}</span>
+      </div>
+    </Dialog>
     <h1 class="p-4">Wygenerowane zestawy</h1>
-    <DataTable :value="products" tableStyle="min-width: 50rem">
-      <Column field="code" header="Code"></Column>
-      <Column field="name" header="Name"></Column>
-      <Column field="category" header="Category"></Column>
-      <Column field="quantity" header="Quantity"></Column>
+    <DataTable :value="data" tableStyle="min-width: 50rem">
+      <Column field="name"/>
+      <Column
+          v-for="part in Object.values(PARTS)"
+          :field="part"
+          :header="HEADINGS[part]"
+      >
+        <template #body="{data, index, field}">
+          {{ data[part] }}
+          <Button v-if="data[part]" @click="displayPartSpec(index, field)" size="small" icon="pi pi-search"></Button>
+        </template>
+      </Column>
+      <Column field="save">
+        <template #body="{index}">
+          <Button @click="saveSet(index)" label="Zapisz" icon="pi pi-save" class="p-button-success"/>
+        </template>
+      </Column>
     </DataTable>
   </div>
 </template>
 
 <script>
+import Button from "primevue/button";
+import {PARTS, HEADINGS} from "@/constants/parts"
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
 import Checkbox from "primevue/checkbox";
 import stepMixin from "@/mixins/stepMixin";
+import {setService} from "@/services/setService";
+import Dialog from 'primevue/dialog';
+
 export default {
   mixins: [stepMixin],
-  components: {Checkbox, DataTable, Column},
+  components: {Dialog, Button, Checkbox, DataTable, Column},
   data() {
     return {
-      products: [
-
-      ],
+      PARTS,
+      HEADINGS,
+      isSpecDialog: false,
+      currentSpec: {},
+      result: [],
       title: "sets",
     }
   },
-  mounted() {
-
+  computed: {
+    data() {
+      return this.result.map(set => {
+        const setParts = set.components
+        const row = {name: set.name}
+        for (const part of Object.values(PARTS)) {
+          if (!setParts[part]) {
+            row[part] = ""
+            continue
+          }
+          row[part] = setParts[part].name
+        }
+        return row
+      })
+    }
+  },
+  methods: {
+    async saveSet(index) {
+      const set = this.result[index]
+      await setService.saveSet(set.index)
+    },
+    displayPartSpec(index, field) {
+      this.currentSpec = this.result[index].components[field]
+      this.isSpecDialog = true
+    }
+  },
+  async mounted() {
+    this.result = await setService.generate()
   }
 }
 </script>
---
+++ frontend/src/constants/parts.ts
@@ -0,0 +1,18 @@
+export const PARTS = {
+    CPU: "cpu",
+    VIDEO_CARD: "videoCard",
+    MOTHERBOARD: "motherboard",
+    HARD_DRIVE: "hardDrive",
+    ADDITIONAL_DRIVE: "additional_drive",
+    POWER_SUPPLY: "powerSupply",
+    PC_CASE: "pcCase",
+}
+export const HEADINGS = {
+    [PARTS.CPU]: "Procesor",
+    [PARTS.VIDEO_CARD]: "Karta graficzna",
+    [PARTS.MOTHERBOARD]: "Płyta główna",
+    [PARTS.HARD_DRIVE]: "Dysk twardy",
+    [PARTS.ADDITIONAL_DRIVE]: "Dodatkowy dysk",
+    [PARTS.POWER_SUPPLY]: "Zasilacz",
+    [PARTS.PC_CASE]: "Obudowa",
+}
