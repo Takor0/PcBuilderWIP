ab82e66355115263091d2c80d864f941327830ab
Tomasz <tomi0985@wp.pl>
2025-01-25 10:42:27 +0100

    redirect to login

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -16,6 +16,7 @@ const router = createRouter({
         },
         {
             path: '',
+            redirect: '/login',
             component: TheContainer,
             children: [
                 {
